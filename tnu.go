efc35d2f81175f0898620b288230602c8178f16c
Devin Buhl <onedr0p@users.noreply.github.com>
2025-01-30 11:42:02 -0500

    feat: use flags instead of positional args (#2)
    * feat: use pflag instead of positional args
    
    Signed-off-by: Devin Buhl <devin@buhl.casa>
    
    * feat: use pflag instead of positional args
    
    Signed-off-by: Devin Buhl <devin@buhl.casa>
    
    * chore: update readme
    
    Signed-off-by: Devin Buhl <devin@buhl.casa>
    
    * chore: update readme
    
    Signed-off-by: Devin Buhl <devin@buhl.casa>
    
    * chore: update readme
    
    Signed-off-by: Devin Buhl <devin@buhl.casa>
    
    ---------
    
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- tnu.go
+++ tnu.go
@@ -22,29 +22,29 @@ import (
 )
 
 func main() {
-	var rebootModeF string
-	flag.StringVarP(&rebootModeF, "reboot-mode", "m", "default", "select the reboot mode during upgrade. Mode powercycle bypasses kexec. Valid values are: default, powercycle.")
+	var (
+		nodeName   string
+		imageTag   string
+		powercycle bool
+	)
+	flag.StringVar(&nodeName, "node", "", "The name of the node to upgrade (required).")
+	flag.StringVar(&imageTag, "tag", "", "The image tag to upgrade to (required).")
+	flag.BoolVar(&powercycle, "powercycle", false, "If set, the machine will reboot using powercycle instead of kexec.")
 	flag.Usage = func() {
-		log.Printf("usage: tnu <node> <tag>\n%s", flag.CommandLine.FlagUsages())
+		log.Printf("usage: tnu --node <node> --tag <tag> [--powercycle]\n%s", flag.CommandLine.FlagUsages())
 	}
 	flag.Parse()
-	if flag.NArg() != 2 {
-		log.Fatalf("usage: tnu <node> <tag>\n%s", flag.CommandLine.FlagUsages())
-	}
-	var rebootMode machineapi.UpgradeRequest_RebootMode
-	switch rebootModeF {
-	case "default":
-		rebootMode = machineapi.UpgradeRequest_DEFAULT
-	case "powercycle":
-		rebootMode = machineapi.UpgradeRequest_POWERCYCLE
-	default:
-		log.Printf("invalid reboot mode: %s", rebootModeF)
-		log.Fatalf("usage: tnu <node> <tag>\n%s", flag.CommandLine.FlagUsages())
+
+	if nodeName == "" || imageTag == "" {
+		log.Fatalf("missing required flags: --node and --tag are required\n%s", flag.CommandLine.FlagUsages())
 	}
 
-	nodename := flag.Arg(0)
-	tag := flag.Arg(1)
-	ctx := client.WithNode(context.Background(), nodename)
+	rebootMode := machineapi.UpgradeRequest_DEFAULT
+	if powercycle {
+		rebootMode = machineapi.UpgradeRequest_POWERCYCLE
+	}
+
+	ctx := client.WithNode(context.Background(), nodeName)
 	c, err := client.New(ctx, client.WithDefaultConfig())
 	if err != nil {
 		panic(err)
@@ -58,8 +58,8 @@ func main() {
 	if !ok {
 		panic("not a k8s.Nodename")
 	}
-	if nn.TypedSpec().Nodename != nodename {
-		log.Fatalf("expected node %s, got %s", nodename, nn.TypedSpec().Nodename)
+	if nn.TypedSpec().Nodename != nodeName {
+		log.Fatalf("expected node %s, got %s", nodeName, nn.TypedSpec().Nodename)
 	}
 
 	r, err = c.COSI.Get(ctx, resource.NewMetadata("config", "MachineConfigs.config.talos.dev", "v1alpha1", resource.VersionUndefined))
@@ -85,18 +85,18 @@ func main() {
 		panic(err)
 	}
 	mcSchematic := ntref.Name()[strings.LastIndex(ntref.Name(), "/")+1:]
-	nodeSchematic := getSchematicAnnotation(ctx, nodename)
-	if vresp.Messages[0].GetVersion().GetTag() == tag && mcSchematic == nodeSchematic {
-		log.Printf("node is up-to-date (schematic: %s, tag: %s)", mcSchematic, tag)
+	nodeSchematic := getSchematicAnnotation(ctx, nodeName)
+	if vresp.Messages[0].GetVersion().GetTag() == imageTag && mcSchematic == nodeSchematic {
+		log.Printf("node is up-to-date (schematic: %s, tag: %s)", mcSchematic, imageTag)
 		os.Exit(0)
 	}
 
-	ntref, err = reference.WithTag(ntref, tag)
+	ntref, err = reference.WithTag(ntref, imageTag)
 	if err != nil {
 		panic(err)
 	}
 
-	log.Printf("upgrading %s to %s", nodename, ntref)
+	log.Printf("upgrading %s to %s", nodeName, ntref)
 	uresp, err := c.UpgradeWithOptions(ctx,
 		client.WithUpgradeImage(ntref.String()),
 		client.WithUpgradePreserve(true),
